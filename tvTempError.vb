99b714c1740555fab2b8c6ee12cb31f76c503ff5
iCloExecutable <59067041+iCloExecutable@users.noreply.github.com>
2022-10-06 19:44:56 +0300

    Delete tvTempError.vb

--- tvTempError.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class tvTempError
-	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-		Dim tvnormal As Form1
-		tvnormal = New Form1()
-		tvnormal.Show()
-		Me.Hide()
-	End Sub
-End Class
